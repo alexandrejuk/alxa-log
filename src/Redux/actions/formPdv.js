8083e7e550a6c120cb71f0b472caf09ff7491a50
Alexandre Soares <ale_santos.soares@hotmail.com>
2021-07-04 22:50:14 -0300

    chore: remove files not used

--- src/Redux/actions/formPdv.js
+++
@@ -1,2 +0,0 @@
-export const SET_FORM_PDV = 'SET_FORM_PDV'
-export const CLEAR_FORM_PDV = 'CLEAR_FORM_PDV'
--- src/Redux/actions/status.js
+++
@@ -1,4 +0,0 @@
-export const SET_STATUS = 'SET_STATUS'
-export const UNSET_STATUS = 'UNSET_STATUS'
-export const SET_NEW_STATUS = 'SET_NEW_STATUS'
-export const SET_UPDATE_STATUS = 'SET_UPDATE_STATUS'
