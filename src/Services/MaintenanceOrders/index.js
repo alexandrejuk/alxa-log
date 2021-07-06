0e4f539b64570b7db0ddf1bfb9f0ffb6519d0cfa
Alexandre Soares <ale_santos.soares@hotmail.com>
2021-07-06 11:28:19 -0300

    chore: update routes

--- src/Services/MaintenanceOrders/index.js
+++ src/Services/MaintenanceOrders/index.js
@@ -1,19 +1,19 @@
 import axiosIntance from '../../utils/axiosInstance'
 
 const getAll = async (params = {}) => {
-  return await axiosIntance.get('/maintenanceOrders', { params })
+  return await axiosIntance.get('/maintenance-orders', { params })
 }
 
 const createMaintenanceOrder = async (values) => {
-  return await axiosIntance.post('/maintenanceOrders', values)
+  return await axiosIntance.post('/maintenance-orders', values)
 }
 
 const updateMaintenanceOrder = async (values) => {
-  return await axiosIntance.put(`/maintenanceOrders/${values.id}`, values)
+  return await axiosIntance.put(`/maintenance-orders/${values.id}`, values)
 }
 
 const getById = async (values) => {
-  return await axiosIntance.get(`/maintenanceOrders/${values.id}`)
+  return await axiosIntance.get(`/maintenance-orders/${values.id}`)
 }
 
 export { 
