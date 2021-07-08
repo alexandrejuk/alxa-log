6b86a14507fda41b5e5fd6e6e23fce6ae4c442cb
Alexandre Soares <ale_santos.soares@hotmail.com>
2021-07-07 23:40:29 -0300

    change to https

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = `http://jls-prd.herokuapp.com/api`
+const baseURL = `https://jls-prd.herokuapp.com//api`
 
 const axiosInstance = axios.create({ baseURL })
 
