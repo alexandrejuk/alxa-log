19c5d113072db09ed89c6e4dadb19f63f0658047
Alexandre Soares <ale_santos.soares@hotmail.com>
2021-07-12 13:12:59 -0300

    find all plates orders

--- src/Pages/MaintenanceManagerMobile/Manager/index.js
+++ src/Pages/MaintenanceManagerMobile/Manager/index.js
@@ -31,10 +31,15 @@ const Manager = ({
   const handleClick = async () => {
     try {
       const { data } = await getByPlate({ plate: searchVehicle })
-      if (data) {
-        return history.push(`/logged/mobile-maintenance-detail/${data.id}`)
-      } else {
-        message.info('Não foi possível localizar nenhuma ordem de manutenção!')
+      if (data.length > 0 && data.length === 1) {
+        return history.push(`/logged/mobile-maintenance-detail/${datap[0].id}`)
+      } 
+      
+      if(data.length > 1) {
+        return history.push(`/logged/mobile-maintenance-list?plate=${searchVehicle}`)
+      }
+      else {
+        return message.info('Não foi possível localizar nenhuma ordem de manutenção!')
       }
     } catch (error) {
       message.error('Não foi possível localizar as manutenções!')
